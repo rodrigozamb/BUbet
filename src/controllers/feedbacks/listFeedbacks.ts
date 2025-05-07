b1d221322a89aa4eba47f151311bba526f591088
rodrigozamb <rodrigo-zamboni@hotmail.com>
2025-05-07 03:01:20 -0300

    fix feedback list route

--- src/controllers/feedbacks/listFeedbacks.ts
+++ src/controllers/feedbacks/listFeedbacks.ts
@@ -5,13 +5,16 @@ import { z } from "zod";
 export async function listFeedBacks(req: FastifyRequest, res: FastifyReply){
 
     const listFeedBacksParamsSchema = z.object({
-        verified: z.coerce.boolean()
+        verified: z.string()
     })
 
-    let {verified } = listFeedBacksParamsSchema.parse(req.params)
-
+    const {verified } = listFeedBacksParamsSchema.parse(req.params)
+    let verify = false
+    if(verified == "true"){
+        verify = true
+    }
     const listFeedBacksUseCase = makeListFeedbacksUseCase()
-    const { feedbacks } = await listFeedBacksUseCase.execute({verified})
+    const { feedbacks } = await listFeedBacksUseCase.execute({verified: verify})
 
     return res.status(201).send(feedbacks)
 }
